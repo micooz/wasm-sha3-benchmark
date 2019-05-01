8b0139c6a0dd02c702f77af7c042ef2db9dde217
micooz <micooz@hotmail.com>
2019-05-02 00:01:44 +0800

    fix: better feedback

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
-    return format!("Hello, {}!", name);
+    return format!(r#"
+Hello visitor: {}<br/>
+If you see this, WebAssembly is running correctly in your browser!"#, name);
 }
